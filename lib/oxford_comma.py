71e97da4f84ff050f9b5230b95939fd1368e00dc
abudhosamuel <abudhosamuel@gmail.com>
2024-09-02 13:26:16 +0300

    join

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return ' and '.join(items)
+    else:
+        return ', '.join(items[:-1]) + f", and {items[-1]}"
+
+print(oxford_comma(["fiddleheads"]))  # => "fiddleheads"
+print(oxford_comma(["fiddleheads", "okra"]))  # => "fiddleheads and okra"
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))  # => "fiddleheads, okra, and kohlrabi"
